07bb9dd69763dd7fe568af24c067de396dfae022
anji-1 <kakarlaanjaneyulunaidu422@gmail.com>
2023-05-04 22:48:30 +0530

    Create a java programme

---
+++ src/Firstjava/Testjava.java
@@ -0,0 +1,9 @@
+package Firstjava;
+public class Testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Create a java programme");
+	}
+
+}
